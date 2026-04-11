e359c6bbd37f2318b788deadaa9b6f96e9e16d1d
Wissniper <wisdom272006@gmail.com>
2026-04-11 16:38:04 +0200

    suppress react-refresh lint warning in context files

--- src/contexts/MapContext.tsx
+++ src/contexts/MapContext.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useRef, useState, ReactNode } from 'react';
 import type { Map as LeafletMap } from 'leaflet';
 
